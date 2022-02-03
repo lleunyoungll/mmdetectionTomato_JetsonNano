7eb6383d66c4f91be70dd623da246b0ea4290414
lleunyoungll <98143576+lleunyoungll@users.noreply.github.com>
2022-02-03 14:32:11 +0900

    Delete toCOCOdata.sh

--- toCOCOdata.sh
+++
@@ -1,4 +0,0 @@
-python labelme2coco.py imgjsonDataset Train --labels labels.txt
-python labelme2coco.py imgjsonDataset Validation --labels labels.txt
-python labelme2coco.py imgjsonDataset Test --labels labels.txt
-echo "all datasets are converted to COCO format completely!"
